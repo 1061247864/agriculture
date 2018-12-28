881827feb7445dcd85676b16c9f1b7abefbde96d
luzhilong <loveluzhilong@qq.com>
2018-12-28 10:36:20 +0800

    addpermission

--- agriculture-api/src/main/java/com/pojo/Permission.java
+++ agriculture-api/src/main/java/com/pojo/Permission.java
@@ -1,9 +1,17 @@
 package com.pojo;
 
+import java.util.Date;
+
 public class Permission {
     private Integer perId;
 
     private String perName;
+
+    private String preMenuName;
+
+    private String preMenuUrl;
+
+    private Date createDate;
 
     public Integer getPerId() {
         return perId;
@@ -19,5 +27,29 @@ public class Permission {
 
     public void setPerName(String perName) {
         this.perName = perName == null ? null : perName.trim();
+    }
+
+    public String getPreMenuName() {
+        return preMenuName;
+    }
+
+    public void setPreMenuName(String preMenuName) {
+        this.preMenuName = preMenuName == null ? null : preMenuName.trim();
+    }
+
+    public String getPreMenuUrl() {
+        return preMenuUrl;
+    }
+
+    public void setPreMenuUrl(String preMenuUrl) {
+        this.preMenuUrl = preMenuUrl == null ? null : preMenuUrl.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
     }
 }
---
+++ agriculture-user-provider/src/main/java/com/mapper/PermissionMapper.java
@@ -0,0 +1,22 @@
+package com.mapper;
+
+import com.pojo.Permission;
+
+public interface PermissionMapper {
+
+
+    int deleteByPrimaryKey(Integer perId);
+
+    int insert(Permission record);
+
+    int insertSelective(Permission record);
+
+
+    Permission selectByPrimaryKey(Integer perId);
+
+
+
+    int updateByPrimaryKeySelective(Permission record);
+
+    int updateByPrimaryKey(Permission record);
+}
