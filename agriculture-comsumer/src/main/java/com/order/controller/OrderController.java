85e436e18aac7285a6896f43fa4e73d5cb587664
luzhilong <loveluzhilong@qq.com>
2018-12-29 11:41:04 +0800

    init

--- agriculture-comsumer/src/main/java/com/order/controller/OrderController.java
+++ agriculture-comsumer/src/main/java/com/order/controller/OrderController.java
@@ -1,8 +1,5 @@
 package com.order.controller;
 
-import java.util.Date;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
